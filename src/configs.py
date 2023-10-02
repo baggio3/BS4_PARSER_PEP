a430a43ef21ffe7083363bbb1348da6c51f9cc4b
Чупрунов Константин <rombuild@yandex.ru>
2023-10-02 11:20:55 +0300

    Initial commit for parser project

---
+++ src/configs.py
@@ -0,0 +1,69 @@
+# configs.py
+import argparse
+from constants import BASE_DIR
+# Новые импорты:
+# модуль для работы с логами,
+import logging
+# хендлер с ротацией логов.
+from logging.handlers import RotatingFileHandler
+
+
+# Описание формата логов:
+# Время записи – Уровень сообщения – Cообщение.
+LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
+# Указываем формат времени.
+DT_FORMAT = '%d.%m.%Y %H:%M:%S'
+
+
+def configure_argument_parser(available_modes):
+    parser = argparse.ArgumentParser(description='Парсер документации Python')
+    parser.add_argument(
+        'mode',
+        choices=available_modes,
+        help='Режимы работы парсера'
+    )
+# Новый аргумент.
+    parser.add_argument(
+        '-c',
+        '--clear-cache',
+        action='store_true',
+        help='Очистка кеша'
+    )
+# Новый аргумент.
+    parser.add_argument(
+        '-o',
+        '--output',
+        choices=('pretty', 'file'),
+        help='Дополнительные способы вывода данных'
+    )
+    # parser.add_argument(
+    #     '-p',
+    #     '--pretty',
+    #     action='store_true',
+    #     help='Вывод в формате PrettyTable'
+    # )
+    return parser
+
+
+def configure_logging():
+    log_dir = BASE_DIR / 'logs'
+    log_dir.mkdir(exist_ok=True)
+    # Отсюда начинается новый код!
+    # Получение абсолютного пути до файла с логами.
+    log_file = log_dir / 'parser.log'
+
+    # Инициализация хендлера с ротацией логов.
+    # Максимальный объём одного файла — десять в шестой степени байт (10**6),
+    # максимальное количество файлов с логами — 5.
+    rotating_handler = RotatingFileHandler(
+        log_file, maxBytes=10 ** 6, backupCount=5
+    )
+    # Базовая настройка логирования basicConfig.
+    logging.basicConfig(
+        datefmt=DT_FORMAT,
+        format=LOG_FORMAT,
+        # Уровень записи логов.
+        level=logging.INFO,
+        # Вывод логов в терминал.
+        handlers=(rotating_handler, logging.StreamHandler())
+    )
---
+++ src/exceptions.py
@@ -0,0 +1,6 @@
+
+
+
+class ParserFindTagException(Exception):
+    """Вызывается, когда парсер не может найти тег."""
+    pass 
